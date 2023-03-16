e370361b456be9800a63a5d188ec49fd2ef88cd9
schwarda <schwartzd14@gmail.com>
2023-03-16 17:44:38 +0100

    macos deleted

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/lopkart/Documents/flutter_projects/http_bloc/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/lopkart/Documents/flutter_projects/http_bloc/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/lopkart/Documents/flutter_projects/http_bloc/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
